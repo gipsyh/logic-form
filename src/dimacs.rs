a6188558c43b509f955505297bd2d54fb959cbbb
Yuheng Su <gipsyh.icu@gmail.com>
2024-01-07 12:36:11 +0800

    update

--- src/dimacs.rs
+++ src/dimacs.rs
@@ -10,21 +10,19 @@ impl Cnf {
     pub fn from_dimacs_str(str: &str) -> Self {
         let mut cnf = Cnf::new();
         for line in str.lines() {
-            let symbols: Vec<&str> = line.split_whitespace().collect();
-            match symbols[0] {
-                "p" => (),
-                "c" => (),
-                _ => {
-                    let mut clause: Vec<i32> =
-                        symbols.iter().map(|&s| s.parse::<i32>().unwrap()).collect();
-                    assert!(clause.pop().unwrap() == 0);
-                    cnf.push(Clause::from_iter(
-                        clause
-                            .into_iter()
-                            .map(|lit| Lit::new(Var::new(lit.unsigned_abs() as _), lit > 0)),
-                    ));
-                }
+            if &line[0..1] == "p" || &line[0..1] == "c" {
+                continue;
             }
+            let mut clause: Vec<i32> = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect();
+            assert!(clause.pop().unwrap() == 0);
+            cnf.push(Clause::from_iter(
+                clause
+                    .into_iter()
+                    .map(|lit| Lit::new(Var::new(lit.unsigned_abs() as _), lit > 0)),
+            ));
         }
         cnf
     }
